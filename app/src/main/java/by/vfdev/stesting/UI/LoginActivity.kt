6cfa9846583d139f63469119419cb8f54080cff1
Vasili <frolov98blr@mail.ru>
2021-07-22 18:31:15 +0300

    Update to 0.0.0 (0)

---
+++ app/src/main/java/by/vfdev/stesting/UI/LoginActivity.kt
@@ -0,0 +1,12 @@
+package by.vfdev.stesting.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.vfdev.stesting.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
