8d3db6a41a716fb3ec199b8bd4c2344831c9aa98
Vasili <frolov98blr@mail.ru>
2021-07-22 19:01:25 +0300

    Update to 0.0.2 (2)

---
+++ app/src/main/java/by/vfdev/stesting/UI/QuestionAdapter.kt
@@ -0,0 +1,54 @@
+package by.vfdev.stesting.UI
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import by.vfdev.stesting.R
+import by.vfdev.stesting.RemoteModel.Question
+
+class QuestionAdapter (private val questionList: ArrayList<Question>) : RecyclerView.Adapter<QuestionAdapter.MyViewHolder> () {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val questionText = itemView.findViewById<TextView>(R.id.tvQuestion)
+        val answerA = itemView.findViewById<TextView>(R.id.rbAnswerA)
+        val answerB = itemView.findViewById<TextView>(R.id.rbAnswerB)
+        val answerC = itemView.findViewById<TextView>(R.id.rbAnswerC)
+        val answerD = itemView.findViewById<TextView>(R.id.rbAnswerD)
+        val answerE = itemView.findViewById<TextView>(R.id.rbAnswerE)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.question_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentitem = questionList[position]
+        holder.questionText.text = currentitem.QuestionText
+        holder.answerA.text = currentitem.AnswerA
+        holder.answerB.text = currentitem.AnswerB
+        holder.answerC.text = currentitem.AnswerC
+        holder.answerD.text = currentitem.AnswerD
+
+        // ??????????????????????????????????????????
+        if (currentitem.AnswerD == "") {
+            holder.answerD.isVisible = false
+        }
+
+        holder.answerE.text = currentitem.AnswerE
+        if (currentitem.AnswerE == "") {
+            holder.answerE.isVisible = false
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+}
